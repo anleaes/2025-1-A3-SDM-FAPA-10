9f1c592998f533603b87b5e9b416f24ec1b1333d
pedrxfraga <pedrohffraga@gmail.com>
2025-06-23 23:00:33 -0300

    implementar products/categories

--- academiaapp/academiaapp/settings.py
+++ academiaapp/academiaapp/settings.py
@@ -34,6 +34,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'categories.apps.CategoriesConfig',
+    'products.apps.ProductsConfig',
+    
 ]
 
 MIDDLEWARE = [
