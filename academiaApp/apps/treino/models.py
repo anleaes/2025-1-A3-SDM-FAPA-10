89e48124418b0a5d2e85a2be9f3fa66577d34d94
pedrxfraga <pedrohffraga@gmail.com>
2025-06-26 19:54:57 -0300

    Treino models

--- academiaApp/apps/treino/models.py
+++ academiaApp/apps/treino/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class Treino(models.Model):
+    exercicio = models.TextField('Exercicio', max_length=50)
+    grupoMuscular = models.TextField('Grupo Muscular', max_length=100)
+    
+    class Meta:
+        verbose_name = 'Personal'
+        verbose_name_plural = 'Personais'
+        ordering =['id']
+
+    def __str__(self):
+        return self.name
