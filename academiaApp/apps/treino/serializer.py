a086df2d897af2c0582ed0d6ff0643e66c322285
pedrxfraga <pedrohffraga@gmail.com>
2025-06-26 19:53:11 -0300

    Treino serializer

---
+++ academiaApp/apps/treino/serializer.py
@@ -0,0 +1,13 @@
+from .models import Personal
+from rest_framework import serializers
+
+class TreinoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Treino
+        fields = '__all__'
+        
+        # Para chamar todos os atributos:
+        # fields = '__all__'
+        
+        # Para chamar somentes os atributos de interesse:
+        # fields = ['id','created_on', 'updated_on', 'name', 'description']
