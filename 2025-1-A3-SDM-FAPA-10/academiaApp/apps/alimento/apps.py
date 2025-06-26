0304964588b652750345177d236a23f4aa89123d
pedrxfraga <pedrohffraga@gmail.com>
2025-06-26 20:51:11 -0300

    alimento models

--- 2025-1-A3-SDM-FAPA-10/academiaApp/apps/alimento/apps.py
+++ 2025-1-A3-SDM-FAPA-10/academiaApp/apps/alimento/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class AlimentoConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'alimento'
+    verbose_name = 'Alimento'
--- 2025-1-A3-SDM-FAPA-10/academiaApp/apps/alimento/models.py
+++ 2025-1-A3-SDM-FAPA-10/academiaApp/apps/alimento/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class Alimento(models.Model):
+    nome = models.TextField('Alimento', max_length=50)
+    tipoAlimento = models.TextField('tipo de alimento', max_length=100)
+    
+    class Meta:
+        verbose_name = 'Alimento'
+        verbose_name_plural = 'Alimentos'
+        ordering =['id']
+
+    def __str__(self):
+        return self.name
