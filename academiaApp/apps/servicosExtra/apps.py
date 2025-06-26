f3fe93b8152c295db8e12a195a77634a0b6a3d39
pedrxfraga <pedrohffraga@gmail.com>
2025-06-26 20:36:16 -0300

    servicos settings url

--- academiaApp/apps/servicosExtra/apps.py
+++ academiaApp/apps/servicosExtra/apps.py
@@ -1,6 +1,7 @@
 from django.apps import AppConfig
 
 
-class ServicosextraConfig(AppConfig):
+class ServicosExtraConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'servicosExtra'
+    verbose_name = 'ServicosExtra'
--- academiaApp/apps/servicosExtra/urls.py
+++ academiaApp/apps/servicosExtra/urls.py
@@ -5,7 +5,7 @@ from rest_framework import routers
 app_name = 'servicosExtra'
 
 router = routers.DefaultRouter()
-router.register('', views.servicosExtraaViewSet, basename='servicosExtra')
+router.register('', views.ServicosExtraViewSet, basename='servicosExtra')
 
 urlpatterns = [
     path('', include(router.urls) )
