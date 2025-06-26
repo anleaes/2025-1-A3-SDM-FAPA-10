f80319bee79ca7736cf2092bda200cfeb1d7e38b
pedrxfraga <pedrohffraga@gmail.com>
2025-06-26 20:21:27 -0300

    conta models

--- academiaApp/apps/conta/models.py
+++ academiaApp/apps/conta/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class Conta(models.Model):
+    Usuario = models.TextField('Usuario', max_length=50)
+    Senha= models.TextField('Senha', max_length=100)
+    
+    class Meta:
+        verbose_name = 'Conta'
+        verbose_name_plural = 'Contas'
+        ordering =['id']
+
+    def __str__(self):
+        return self.name
